6f81b47f7befdac37b32b8bb6f9ae9b876e2b899
Juakata <juakata07@gmail.com>
2020-05-18 14:54:45 -0600

    add axios to App component to fetch the data

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+import unsplash from '../api/unsplash';
 
 class App extends React.Component {
+  state = { images: [], error: '' }
   onSearchBarSubmit = term => {
-    console.log(term);
+    unsplash.get('/search/photos', {
+      params: { query: term },
+    })
+    .then(response => this.setState({ images: response.data.results }))
+    .catch(error => this.setState({ error }));
   }
 
   render() {
+    const { images, error } = this.state;
     return (
       <div className="ui container">
+        <div>{ error }</div>
         <SearchBar onSearchBarSubmit={this.onSearchBarSubmit}/>
+        <ImageList images={images} />
       </div>
     );
   }
